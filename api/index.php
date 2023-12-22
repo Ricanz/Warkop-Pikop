edf10da8b38d5023ecdd55af286c61829d4db450
Riyanti Maulya <87960308+Ricanz@users.noreply.github.com>
2023-12-22 14:17:36 +0700

    cors

--- api/index.php
+++ api/index.php
@@ -1,4 +1,9 @@
 <?php
+// Set header untuk mengizinkan CORS
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
 include('api.php');
 include('../config/function_library.php');
 
